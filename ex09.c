cda7b3ec6661b7b9d5488d3f3781ef50fca8a1b8
AmerZakret <162097991+AmerZakret@users.noreply.github.com>
2024-11-04 20:52:07 +0300

    Update ex09.c

--- ex09.c
+++ ex09.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+int main() {
+  int sum = 0;
+  int num = 0;
+  double avg = 0;
+  int count = 0;
+
+  while(1) {
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    
+    if (num == -1) {
+      break;
+    }
+    else {
+    sum += num;
+    count++;
+    }
+  }
+  avg = (double) sum / count;
+  printf("Average: %.1f\n", avg);
+  printf("Sum: %d\n", sum);
+}
 
