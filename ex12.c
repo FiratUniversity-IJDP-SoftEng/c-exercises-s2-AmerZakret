18855e1f5a2e07ed952da60d08ad8340be4b7d9d
AmerZakret <162097991+AmerZakret@users.noreply.github.com>
2024-11-04 20:55:10 +0300

    Update ex12.c

--- ex12.c
+++ ex12.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+int main() {
+  int num;
+  long long factorial = 1;
+
+  do {
+    printf("Please enter a number (less than 50):");
+    scanf("%d", &num);
+
+    if (num >= 50 || num <= 0) {
+        printf("Invalid number\n");
+    }
+    
+  }
+  while (num >= 50 || num <= 0);
+
+  for(int i = 1; i <= num; i++) {
+    factorial *= i;
+  }
+
+  printf("The factorial of the number is %llu\n", factorial);
+}
 
