d0dd47359986fb35ed60888c2f24abb3722b287c
AmerZakret <162097991+AmerZakret@users.noreply.github.com>
2024-11-04 20:55:44 +0300

    Update ex13.c

--- ex13.c
+++ ex13.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+long long factorialFunc(num) {
+  for (int i = 1; i <= num; i++) {
+
+    if (num == 0) {
+      printf("The factorial of 0 is 1.");
+      return 1;
+    }
+    else {
+      return num * factorialFunc(num - 1);
+    }
+  }
+}
+
+int main(){
+  int num;
+  long long factorial = 1;
+
+  do {
+    printf("Please enter a number (less than 50):");
+    scanf("%d", &num);
+
+    if (num >= 50 || num <= 0) {
+        printf("Invalid number\n");
+    }
+    
+  } while (num >= 50 || num <= 0);
+
+  printf("The factorial of the number is %llu\n", factorialFunc(num));
+}
 
