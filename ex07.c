4f3cac29411611b5179a22162ed2b3708473708e
AmerZakret <162097991+AmerZakret@users.noreply.github.com>
2024-11-04 20:46:31 +0300

    Update ex07.c

--- ex07.c
+++ ex07.c
@@ -1,1 +1,21 @@
+int main() {
+  for (int i = 1; i < 11; i++) {
+    for (int j = 0; j < 3; j++) {
+      if (j == 0) {
+        int value = i * 5;
+        printf("%d  ", value);
+      }
+      else if (j == 1) {
+        int value = 100-((i-1)*10+1);
+        printf("%d  ", value);
+      }
+      else
+      {
+        printf("%d", i);
+      }
+    }
+    printf("\n");
+  }
+
+}
 
