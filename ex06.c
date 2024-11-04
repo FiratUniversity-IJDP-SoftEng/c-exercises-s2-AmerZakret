555225d185a4c341c283678d4b9141a4c1bf81c2
AmerZakret <162097991+AmerZakret@users.noreply.github.com>
2024-11-04 20:46:05 +0300

    Update ex06.c

--- ex06.c
+++ ex06.c
@@ -1,1 +1,17 @@
+int main() {
+
+int array1[17];
+int lenght = 0;
+
+for(int i = 100; i >= 50; i--) {
+  if (i % 3 == 0) {
+    array1[lenght++] = i;
+  }
+}
+
+for(int i = 0; i < lenght; i++) {
+  printf("%d ,", array1[i]);
+
+}
+}
 
