aed63e37c466d833bfc8f915a2f041923ca97100
AmerZakret <162097991+AmerZakret@users.noreply.github.com>
2024-11-04 20:53:15 +0300

    Update ex10.c

--- ex10.c
+++ ex10.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+int main() {
+  int value;
+
+  for (int i = 1; i < 11; i++) {
+    for(int j = 1; j < 11; j++) {
+      value = i * j;
+      printf("%d ", value);
+    }
+    printf("\n");
+  }
+}
 
